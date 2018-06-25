f44f37859b50a25d2844fd494e00dfd606f3000a
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-06-25 06:55:45 +0200

    Exact match for host. Partially fixes #2

--- connectors/amqp/schema/schema.go
+++ connectors/amqp/schema/schema.go
@@ -5,7 +5,7 @@ import "github.com/cloudfoundry-community/gautocloud/decoder"
 type AmqpSchema struct {
 	Uri      decoder.ServiceUri `cloud:"ur(i|l),regex"`
 	Port     int                `cloud:"" cloud-default:"5672"`
-	Host     string             `cloud:"host.*,regex" cloud-default:"localhost"`
+	Host     string             `cloud:"hostname" cloud-default:"localhost"`
 	User     string             `cloud:".*user.*,regex" cloud-default:"root"`
 	Password string             `cloud:".*pass.*,regex"`
 	Vhost    string             `cloud:"vhost.*,regex"`
